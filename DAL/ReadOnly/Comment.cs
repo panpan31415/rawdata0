2f5176f4d1aef6f5f85082bd8f9c1b0782bfd612
panpan31415 <panpan31415@gmail.com>
2015-11-19 11:37:33 +0100

    add 2 folder

---
+++ DAL/ReadOnly/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public int postId { get; set; }
+        public string text { get; set; }
+        public DateTime creationDate { get; set; }
+        public int userid { get; set; }
+    }
+}
