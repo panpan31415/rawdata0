a95463dc919dcc26227c00023a5640f9bfe6f1ae
panpan31415 <panpan31415@gmail.com>
2015-11-19 12:34:29 +0100

    noting

---
+++ DAL/ReadOnly/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public interface IReadOlnyRepository<T> where T : class, new()
+    {
+        /// <summary>
+        /// Get records from database as a collection
+        /// </summary>
+        /// <param name="limit">specify the size of the returned collection </param>
+        /// <param name="offset">specify the start point of cursor in a table that will be selected.</param>
+        /// <returns>a collection of records that selected from database </returns>
+        IEnumerable<T> get(int limit = 10, int offset = 0);
+        T getById(int id);
+    }
+}
---
+++ DAL/ReadOnly/abstractRepository.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace DAL.ReadOnly
+{
+    public abstract class abstractRepository<T> : IReadOlnyRepository<T> where T : class, new()
+    {
+        public abstract IEnumerable<T> get(int limit = 10, int offset = 0);
+        public abstract T getById(int id);
+
+        public MySqlDataReader dataReader(string sql)
+        {
+            using (var connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["remote"].ConnectionString))
+            {
+                connection.Open();
+                var cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = sql;
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                return rdr;
+            }
+        }
+
+
+    }
+}
