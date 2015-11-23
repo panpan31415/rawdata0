92862179340f2d1b0498b897224449ca7f75f748
Ioana Grozav <peacelovetheysay@gmail.com>
2015-11-23 15:57:10 +0100

    added repo interface and abstract

---
+++ DAL/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+	public interface IRepository<T> where T : class, IIdentityField
+	{
+		IDataMapper<T> DataMapper { get; }
+		T GetById(long id);
+		IEnumerable<T> GetAll(int limit=10, int offset = 0);
+	}
+}
