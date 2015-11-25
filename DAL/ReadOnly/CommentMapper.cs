35baf06b092a8c79fccd2a4e856ebe8dcaf33a8a
Ioana Grozav <peacelovetheysay@gmail.com>
2015-11-25 13:25:46 +0100

    fixed comment mapper

--- DAL/ReadOnly/CommentMapper.cs
+++ DAL/ReadOnly/CommentMapper.cs
@@ -11,37 +11,39 @@ namespace DAL.ReadOnly
 	{
 		public CommentMapper(string connectionSting) : base(connectionSting)
 		{
-			TableName = "Comment";
+			TableName = "comment";
 			Attributes = new string[] { "postid", "text", "creationDate", "userid" };
 		}
 
 		public override Comment Map(MySqlDataReader reader)
 		{
-
-			int id = 0;
-			int postId = 0;
-			string text = "no text";
-			DateTime creationDate = DateTime.Now;
-			int userid = 0;
-			if (!reader.IsDBNull(0))
-				id = reader.GetInt32(0);
-			if (!reader.IsDBNull(1))
-				postId = reader.GetInt32(1);
-			if (!reader.IsDBNull(2))
-				text = reader.GetString(2);
-			if (!reader.IsDBNull(3))
-				creationDate = reader.GetDateTime(3);
-			if (!reader.IsDBNull(4))
-				userid = reader.GetInt32(4);
-			return new Comment
+			if (reader.HasRows && reader.Read())
 			{
-				Id = id,
-				PostId = postId,
-				Text = text,
-				CreationDate = creationDate,
-				Userid = userid,
-			};
-
+				int id = 0;
+				int postId = 0;
+				string text = "no text";
+				DateTime creationDate = DateTime.Now;
+				int userid = 0;
+				if (!reader.IsDBNull(0))
+					id = reader.GetInt32(0);
+				if (!reader.IsDBNull(1))
+					postId = reader.GetInt32(1);
+				if (!reader.IsDBNull(2))
+					text = reader.GetString(2);
+				if (!reader.IsDBNull(3))
+					creationDate = reader.GetDateTime(3);
+				if (!reader.IsDBNull(4))
+					userid = reader.GetInt32(4);
+				return new Comment
+				{
+					Id = id,
+					PostId = postId,
+					Text = text,
+					CreationDate = creationDate,
+					Userid = userid,
+				};
+			}
+			else return null;
 		}
 	}
 }
