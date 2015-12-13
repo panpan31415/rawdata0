02ed6b3e6012d53c9382d9ed13c1d91209a60661
Ioana <peacelovetheysay@gmail.com>
2015-12-13 17:08:15 +0100

    bootstrap added

--- portfolio2gr4/App/main.js
+++ portfolio2gr4/App/main.js
@@ -1,31 +1,31 @@
 ﻿var my = my || {};
 
 my.viewModel = (function () {
-    var currentMenu = ko.observable("");
-    var menuItems = ["Users", "Annotations", "Categories"],
-        currentView = ko.observable("users_view"),
-        data = ko.observableArray([]),
-        showContent = function (menu) {
-            var name = menu.toLowerCase();
-            currentView(name + "_view");
-            currentMenu(menu);
-            $.getJSON("http://localhost:3133/api/" + name, function (result) {
-                data(result);
-            });
-        },
-        isActive = function(menu) {
-            return menu === currentMenu();
-        },
-    showList = ko.observable(true)
-        ;
-    return {
-        showContent: showContent,
-        currentView: currentView,
-        data: data,
-        menuItems: menuItems,
-        isActive: isActive,
-        showList: showList
-    };
+	var currentMenu = ko.observable("");
+	var menuItems = ["Users", "Annotations", "Categories"],
+		currentView = ko.observable("users_view"),
+		data = ko.observableArray([]),
+		showContent = function (menu) {
+			var name = menu.toLowerCase();
+			currentView(name + "_view");
+			currentMenu(menu);
+			$.getJSON("http://localhost:3133/api/" + name, function (result) {
+			data(result);
+			});
+		},
+		isActive = function (menu) {
+			return menu === currentMenu();
+		},
+	showList = ko.observable(true)
+	;
+	return {
+		showContent: showContent,
+		currentView: currentView,
+		data: data,
+		menuItems: menuItems,
+		isActive: isActive,
+		showList: showList
+	};
 }());
 
 my.viewModel.showContent("users");
