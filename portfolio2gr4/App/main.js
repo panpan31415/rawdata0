0b033e3c27d6229227f7b36ff0d5ceed783be814
Panpan Zhang <panpan31415@gmail.com>
2015-12-17 01:04:18 +0100

    position of search box is fixed , autoclolaps is added to menu

--- portfolio2gr4/App/main.js
+++ portfolio2gr4/App/main.js
@@ -1,45 +1,52 @@
-﻿var masterVM = masterVM || (function ()
-{
+﻿var masterVM = masterVM || (function () {
     this.navigationViewModel = navigationViewModel;
     this.searchViewModel = searchViewModel;
 })();
 var navigationViewModel = (function () {
     var currentMenu = ko.observable("");
-    var menuItems = ["Users", "Annotations", "Categories", "History"],
+    var menuItems = ["Questions", "Users", "Annotations", "Categories", "History"],
 		currentView = ko.observable("users_view"),
 		data = ko.observableArray([]),
 		history = ko.observableArray([]),
-		showContent = function (menu) {
-		    var name = menu.toLowerCase();
-		    currentView(name + "_view");
-		    currentMenu(menu);
-		    if (name === "history") {
-		        name = "users/108/historys";
-		        history([]);
-		        $.getJSON("http://localhost:3133/api/" + name, function (result) {
+        showContent = function (menu) {
+            var toggle = $("#navbar-toggle-button").attr("aria-expanded");
+            if (toggle = (toggle === "true")) {
+                //alert(toggle);
+                $("#navbar-collapse").click(function () {
+                    $("#navbar-toggle-button").trigger('click');
+                });
+            }
+            var name = menu.toLowerCase();
+            currentView(name + "_view");
+            currentMenu(menu);
 
-		            for (var i = 0; i < result.length; i++) {
-		                history.push(new HistoryItem(result[i]));
-		                //console.log("res:");
-		                //console.log(history());
-		            };
+            if (name === "history") {
+                name = "users/108/historys";
+                history([]);
+                $.getJSON("http://localhost:3133/api/" + name, function (result) {
 
-		        });
-		    } else {
-		        $.getJSON("http://localhost:3133/api/" + name, function (result) {
-		            //console.log(result);
-		            data(result);
-		            //console.log("res:");
-		            //console.log(data());
-		        });
-		    }
+                    for (var i = 0; i < result.length; i++) {
+                        history.push(new HistoryItem(result[i]));
+                        //console.log("res:");
+                        //console.log(history());
+                    };
 
-		},
+                });
+            } else {
+                $.getJSON("http://localhost:3133/api/" + name, function (result) {
+                    //console.log(result);
+                    data(result);
+                    //console.log("res:");
+                    //console.log(data());
+                });
+            }
+
+        },
 		isActive = function (menu) {
 		    return menu === currentMenu();
 		},
-	showList = ko.observable(true)
-    ;
+	showList = ko.observable(true);
+    
 
     function HistoryItem(data) {
         var self = this;
